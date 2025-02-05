96e9459e078a180f91de15ed2800c341dee6dcfb
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2025-02-05 23:54:44 +0530

    Create Check if one string swap can make strings equal.java

---
+++ Check if one string swap can make strings equal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int c=0;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i])
+                return false;
+        }
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                c++;
+            }
+        }
+        if(c==0 || c==2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
